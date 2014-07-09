5005a046af0ff832c01c6b7d88ebfb7a64013a88
David Naylor <dnaylor@cs.cmu.edu>
2014-07-09 18:35:22 +0200

    Mock selenium submodules for ReadTheDocs

--- webloader/docs/source/conf.py
+++ webloader/docs/source/conf.py
@@ -40,8 +40,13 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'selenium', 'common.exceptions',
-    'webdriver.support.ui']
+MOCK_MODULES = ['numpy', 'selenium',
+    'selenium.common',
+    'selenium.common.exceptions',
+    'selenium.webdriver',
+    'selenium.webdriver.support',
+    'selenium.webdriver.support.ui',
+]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
