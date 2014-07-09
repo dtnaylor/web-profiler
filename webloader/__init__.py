87db0c2384ad5d06e0722ab5d6ec3140259bf9e4
David Naylor <dnaylor@cs.cmu.edu>
2014-07-09 18:14:14 +0200

    Added FirefoxLoader to webmodule

--- webloader/__init__.py
+++ webloader/__init__.py
@@ -1,5 +1,6 @@
 from loader import Loader, PageResult, LoadResult
 from phantomjs_loader import PhantomJSLoader
 from chrome_loader import ChromeLoader
+from firefox_loader import FirefoxLoader
 
-__all__ = ['Loader', 'PageResult', 'LoadResult', 'PhantomJSLoader', 'ChromeLoader']
+__all__ = ['Loader', 'PageResult', 'LoadResult', 'PhantomJSLoader', 'ChromeLoader', 'FirefoxLoader']
