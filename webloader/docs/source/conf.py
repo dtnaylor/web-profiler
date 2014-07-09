764eed060def745d5ecdb83b96a587d7ebdf6518
David Naylor <dnaylor@cs.cmu.edu>
2014-07-09 18:16:12 +0200

    Mocked out selenium for ReadTheDocs

--- webloader/docs/source/conf.py
+++ webloader/docs/source/conf.py
@@ -40,7 +40,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy', 'selenium']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
